abc123ea49e85861e97a23ffc4e51812696c0df5
Ardahan Dikmedas <arda75.ad@gmail.com>
2021-07-05 20:24:51 +0300

    validate added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,33 @@
-function validate(){
-    if(document.valForm.fname.value == ""){
-        document.getElementById('nameError').style.display = 'block';
+function validation() {
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var error_message = document.getElementById("error_message");
+    var text;
+
+    error_message.style.padding = "10px";
+
+    if(fname == ""){
+        text = "isim gir";
+        error_message.innerHTML = text;
         return false;
     }
-   
-}
+    if(lname == ""){
+        text = "soyisim gir";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if(email == ""){
+        text = "email gir";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if(isNaN(phone) || phone.length != 10){
+        text = "telefon gir";
+        error_message.innerHTML = text;
+        return false;
+    }
+
+
+}    
