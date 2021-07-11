b752b2b0aaaaf8c4a6f26b970592c6ce912b8087
Ardahan Dikmedas <arda75.ad@gmail.com>
2021-07-11 12:35:06 +0300

    gulp added

--- gulpfile.js
+++ gulpfile.js
@@ -25,36 +25,3 @@ gulp.task('default', gulp.series(['browsersync']));
 
 
 
-/*
-var gulp = require('gulp');
-var livereload = require('gulp-livereload');
-var connect = require('gulp-connect');
-
-livereload({ start: true })
-
-gulp.task('css', function(){
-  return gulp.src('./src/*.css')
-    .pipe(livereload());
-});
-
-gulp.task('html', function(){
-  return gulp.src('./src/*.html')
-    .pipe(livereload());
-})
-
-gulp.task('connect', function() {
-  connect.server({
-    root: './src',
-    livereload: true,
-  });
-});
-
-gulp.task('watch', function(){
-  livereload.listen();
-  gulp.watch('./src/*.css', gulp.series(['css']));
-  gulp.watch('./src/*.html', gulp.series(['html']));
-
-});
-
-gulp.task('default', gulp.series(['connect', 'css','html', 'watch']));
-*/
